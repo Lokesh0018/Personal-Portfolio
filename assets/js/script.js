f5bd8e9143497d72bc34b0078e5aad5694a23385
Lokesh0018 <munakalalokesh222@gmail.com>
2026-05-28 09:45:21 +0530

    feat: add skills showcase section

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,27 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 if (sidebarBtn && sidebar) {
   sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 }
+
+// Page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// Add click event to all navigation links
+if (navigationLinks.length > 0 && pages.length > 0) {
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+      const targetPage = this.innerHTML.toLowerCase();
+      
+      for (let j = 0; j < pages.length; j++) {
+        if (targetPage === pages[j].dataset.page) {
+          pages[j].classList.add("active");
+          navigationLinks[j].classList.add("active");
+          window.scrollTo(0, 0);
+        } else {
+          pages[j].classList.remove("active");
+          navigationLinks[j].classList.remove("active");
+        }
+      }
+    });
+  }
+}
